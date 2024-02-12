068c7a00b5052e0a1b0b8d906f99261b0bbf7952
nnicolas <nicolas.nguyenvanthanh@ynov.com>
2024-02-12 17:21:44 +0100

    ajout des commentaires sur tout le css

--- Hangman-web/Server/launch.go
+++ Hangman-web/Server/launch.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 )
 
+// Hangman structure représente l'état du jeu de pendu.
 type Hangman struct {
 	WordToGuess string
 	HiddenWord  []string
@@ -20,18 +21,26 @@ type Hangman struct {
 	Suggest     string
 }
 
+// Data contient l'état actuel du jeu.
 var Data Hangman
 
+// Display gère la logique d'affichage de la page d'accueil du jeu de pendu.
 func Display(w http.ResponseWriter, r *http.Request) {
+	// Parse le fichier HTML du template.
 	custTemplate, err := template.ParseFiles("Hangman-web/templates/accueil.html")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+
+	// Récupère la valeur du bouton et du nom d'utilisateur depuis le formulaire.
 	buttonValue := r.FormValue("bouton")
 	username := r.FormValue("nom_utilisateur")
+
+	// Traite la valeur du bouton.
 	if buttonValue != "" {
 		fmt.Printf("Le Bouton prends une valeur : %s\n", buttonValue)
+		// Initialise les données pour une nouvelle partie.
 		Data.WordToGuess = Func.Random(buttonValue)
 		Data.WordToGuess = Func.ToUpper(Data.WordToGuess)
 		Data.HiddenWord = Func.Hiderr(Data.WordToGuess)
@@ -40,29 +49,37 @@ func Display(w http.ResponseWriter, r *http.Request) {
 		Data.Used = []string{""}
 		Data.Jose = Func.DisplayImage(w, r, Data.Tries)
 		Data.Suggest = ""
-
 	}
 
+	// Traite le nom d'utilisateur.
 	if username != "" {
 		fmt.Printf("Le pseudo de l'utilisateur est  : %s\n", username)
 		Data.Pseudo = username
+		// Redirige vers la page principale du jeu.
 		http.Redirect(w, r, "/index", http.StatusFound)
 	}
 
+	// Exécute le template avec les données actuelles.
 	err = custTemplate.Execute(w, Data)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-
 }
+
+// DisplayIndex gère la logique d'affichage de la page principale du jeu de pendu.
 func DisplayIndex(w http.ResponseWriter, r *http.Request) {
+	// Parse le fichier HTML du template.
 	custTemplate, err := template.ParseFiles("Hangman-web/templates/index.html")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+
+	// Récupère la lettre entrée par l'utilisateur depuis le formulaire.
 	lettre := r.FormValue("lettre")
+
+	// Traite la lettre entrée.
 	if lettre != "" {
 		Data.Suggest = ""
 		if Func.IsAlpha(lettre) == true {
@@ -77,17 +94,20 @@ func DisplayIndex(w http.ResponseWriter, r *http.Request) {
 				if lettre != "" {
 					Data.WordToGuess, Data.Tries, Data.Result = Func.Compare(Data.WordToGuess, Data.Tries, Data.HiddenWord, lettre)
 					if Data.Result == "Win" {
+						// Redirige vers la page de victoire.
 						http.Redirect(w, r, "/win", http.StatusFound)
 					} else if Data.Result == "loose" {
+						// Redirige vers la page de défaite.
 						http.Redirect(w, r, "/loose", http.StatusFound)
 					}
-
 				}
-
 			}
 		}
+		// Met à jour l'image du pendu.
 		Data.Jose = Func.DisplayImage(w, r, Data.Tries)
 	}
+
+	// Exécute le template avec les données actuelles.
 	err = custTemplate.Execute(w, Data)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -95,24 +115,16 @@ func DisplayIndex(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// DisplayWin gère la logique d'affichage de la page de victoire.
 func DisplayWin(w http.ResponseWriter, r *http.Request) {
+	// Parse le fichier HTML du template.
 	custTemplate, err := template.ParseFiles("Hangman-web/templates/win.html")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	err = custTemplate.Execute(w, Data)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-}
-func DisplayLoose(w http.ResponseWriter, r *http.Request) {
-	custTemplate, err := template.ParseFiles("Hangman-web/templates/loose.html")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
+
+	// Exécute le template avec les données actuelles.
 	err = custTemplate.Execute(w, Data)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -120,14 +132,38 @@ func DisplayLoose(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// DisplayLoose gère la logique d'affichage de la page de défaite.
+func DisplayLoose(w http.ResponseWriter, r *http.Request) {
+	// Parse le fichier HTML du template.
+	custTemplate, err := template.ParseFiles("Hangman-web/templates/loose.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Exécute le template avec les données actuelles.
+	err = custTemplate.Execute(w, Data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+// StartServer initialise le serveur HTTP pour le jeu de pendu.
 func StartServer() {
 	println("server started on http://localhost:8080/")
+	
+	// Gère les chemins pour les fichiers CSS et les images.
 	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("./Hangman-web/templates/css/"))))
 	http.Handle("/images/", http.StripPrefix("/images/", http.FileServer(http.Dir("./Hangman-web/assets/images/"))))
+	
+	// Définit les gestionnaires de routes pour les différentes pages.
 	http.HandleFunc("/", Display)
 	http.HandleFunc("/index", DisplayIndex)
 	http.HandleFunc("/win", DisplayWin)
 	http.HandleFunc("/loose", DisplayLoose)
+	
+	// Lance le serveur sur le port 8080.
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		return
